e3fb15ac02ca08456e5b51f29aa6d6316aea26cc
yankeexlr <donttrytobrute@protonmail.ch>
2023-10-09 23:18:05 +0300

    Comments

--- bot/bot.js
+++ bot/bot.js
@@ -1,13 +1,16 @@
+// Requires Telegraf, which is the top SDK for Telegram bots
+// Don't forget to get a new bot / bot token via @BotFather on Telegram
 const { Telegraf } = require("telegraf");
 const TOKEN = 'YOUR_BOT_TOKEN_HERE';
 const bot = new Telegraf(TOKEN);
 
-
+// A link to the mini-app you've set up via @BotFather when creating the app
 const web_link = 'https://t.me/FlipnoteBot/note';
 
-bot.start((ctx) => ctx.reply('Welcome', {
+//
+bot.start((ctx) => ctx.reply('Ready to take some notes?', {
     reply_markup: {
-        inline_keyboard:[ [ {text:'webapp', url: web_link}]]
+        inline_keyboard:[ [ {text:'Open Flipnote', url: web_link}]]
     },
 }) )
 
