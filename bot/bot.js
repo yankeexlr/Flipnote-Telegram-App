9047f5712948955a672f387f06bf8ab2320b52c5
yankeexlr <donttrytobrute@protonmail.ch>
2023-10-09 23:19:30 +0300

    Comments

--- bot/bot.js
+++ bot/bot.js
@@ -7,7 +7,7 @@ const bot = new Telegraf(TOKEN);
 // A link to the mini-app you've set up via @BotFather when creating the app
 const web_link = 'https://t.me/FlipnoteBot/note';
 
-//
+// Make the bot that hosts your mini-app reply with a message on /start and put a link to the mini-app
 bot.start((ctx) => ctx.reply('Ready to take some notes?', {
     reply_markup: {
         inline_keyboard:[ [ {text:'Open Flipnote', url: web_link}]]
